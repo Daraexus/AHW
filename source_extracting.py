18a92c3fd05f331f037c61a87b0504623cbfd813
Tina Peters <cmp346@drexel.edu>
2015-10-01 13:08:31 -0400

    updated notebook and changed the byteswap

--- source_extracting.py
+++ source_extracting.py
@@ -12,7 +12,7 @@ def extract_sources(data):
 	try:
 		bkg = sep.Background(data)
 	except ValueError:
-		data = data.byteswap(True).newbyteorder()
+		data = data.byteswap().newbyteorder()
 		bkg = sep.Background(data)
 	back = bkg.back()
 	data_woback = data-back
@@ -34,30 +34,34 @@ def point_within_distance(existing_points, new_points):
 			new_objects[i] = True
 	return new_objects
    
-def plot_frame(data_array,known_objects,xs,ys,num,ifile,pngout=""):
+def plot_frame(data_array,known_objects,xs,ys,new_objects,num,ifile,vmin, vmax, pngout=""):
 	# If not specified, "pngout" = "", so make a default.
 	if pngout == "":
 		pngout = (os.path.splitext(os.path.basename(ifile))[0] +
 			"_detsources.png")
 	plt.figure(1)
-	plt.imshow(data_array)
+	plt.imshow(data_array, vmin=vmin, vmax=vmax, cmap=plt.get_cmap('gray'))
 	for i in range(len(known_objects)):
-		plt.plot(known_objects[i][0],known_objects[i][1],'ro')
-	for i in range(len(xs)):
-		plt.plot(xs[i],ys[i],'ko')
+		plt.plot(known_objects[i][0],known_objects[i][1],'ko')
+	for i in range(len(xs[~new_objects])):
+		plt.plot(xs[~new_objects][i],ys[~new_objects][i],'go')
+	for i in range(len(xs[new_objects])):
+		plt.plot(xs[new_objects][i],ys[new_objects][i],'ro')
 	plt.savefig(os.path.splitext(pngout)[0] + "_" + num + ".png")
 	plt.clf()
 	return
 
 def find_all_objects(framecube, bad_frame_mask, frame_wcs, pngout, ifile):
+	vmin, vmax = np.min(framecube), np.max(framecube)
 	all_extracted_objects = np.array([])
 	for i in range(framecube.shape[0]):
 		objects = extract_sources(framecube[i,:,:])
-		plot_frame(framecube[i,:,:],all_extracted_objects,objects['x'],objects['y'],str(i),ifile,pngout=pngout)
 		if len(objects) > 0:
 			if len(all_extracted_objects) > 0:
 				new_objects = point_within_distance(all_extracted_objects,np.asarray([objects['x'],objects['y']]).T) #returns array of True and False
 				all_extracted_objects = np.vstack((all_extracted_objects, np.asarray([objects['x'][new_objects],objects['y'][new_objects]]).T))
 			else:
+				new_objects = np.asarray([False]*len(objects))
 				all_extracted_objects = np.asarray([objects['x'],objects['y']]).T
+		plot_frame(framecube[i,:,:],all_extracted_objects,objects['x'],objects['y'],new_objects,str(i),ifile, vmin, vmax, pngout=pngout)
 	return all_extracted_objects
